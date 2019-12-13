dc472105d37d29c2eb41320162b2118408ef025e
谭吉 <99425549@qq.com>
2019-12-13 23:15:36 +0800

    默认适配器模式

---
+++ refueltwo003-designpattern01-adapterpattern01/src/main/java/com/refueltwo/main/Refueltwo003Designpattern01Adapterpattern02Application.java
@@ -0,0 +1,12 @@
+package com.refueltwo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Refueltwo003Designpattern01Adapterpattern02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Refueltwo003Designpattern01Adapterpattern02Application.class, args);
+    }
+}
